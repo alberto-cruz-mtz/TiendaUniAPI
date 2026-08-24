3c5d148e4514cbce62ca1381be748c9fca819615
jose alberto cruz mtz <albertocruz8133@proton.me>
2026-08-23 20:20:57 -0600

    build(init): initialize Spring boot project with default dependencies
    Set up the initial project skeleton including the following dependencies:
    - Spring Web
    - Spring Data JPA
    - Spring Security
    - Spring Boot Actuator
    - Lombok
    - PostgreSQL Driver
    - Validation

---
+++ src/main/java/alberto/cruz/tiendauniapi/TiendaUniApiApplication.java
@@ -0,0 +1,13 @@
+package alberto.cruz.tiendauniapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaUniApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaUniApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alberto/cruz/tiendauniapi/TiendaUniApiApplicationTests.java
@@ -0,0 +1,13 @@
+package alberto.cruz.tiendauniapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaUniApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
