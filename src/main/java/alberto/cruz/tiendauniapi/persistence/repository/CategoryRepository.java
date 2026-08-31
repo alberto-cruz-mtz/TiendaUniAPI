f71589dbe14809d75ab492ca6c999b84938d3487
jose alberto cruz mtz <albertocruz8133@proton.me>
2026-08-30 22:00:41 -0600

    feat(repository): add findByName in CategoryRepository and tenancy methods in ProductRepository

--- src/main/java/alberto/cruz/tiendauniapi/persistence/repository/CategoryRepository.java
+++ src/main/java/alberto/cruz/tiendauniapi/persistence/repository/CategoryRepository.java
@@ -1,7 +1,12 @@
 package alberto.cruz.tiendauniapi.persistence.repository;
 
 import alberto.cruz.tiendauniapi.persistence.entity.CategoryEntity;
+import alberto.cruz.tiendauniapi.persistence.entity.CategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
+    Optional<CategoryEntity> findByName(CategoryName name);
 }
