ee016905733326e293e59c48e9c9815ea6159f21
jose alberto cruz mtz <albertocruz8133@proton.me>
2026-08-30 22:02:31 -0600

    feat(advice): handle InvalidArgumentException in GlobalExceptionHandler

--- src/main/java/alberto/cruz/tiendauniapi/presentation/advice/ProductExceptionHandler.java
+++ src/main/java/alberto/cruz/tiendauniapi/presentation/advice/ProductExceptionHandler.java
@@ -1,4 +1,37 @@
 package alberto.cruz.tiendauniapi.presentation.advice;
 
+import alberto.cruz.tiendauniapi.service.exception.CategoryNotFoundException;
+import alberto.cruz.tiendauniapi.service.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+
+@RestControllerAdvice
 public class ProductExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleProductNotFoundException(ProductNotFoundException exception) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
+        problemDetail.setTitle("Product Not Found");
+        problemDetail.setType(URI.create(GlobalExceptionHandler.DOMAIN_URI + "/product-not-found"));
+
+        return ResponseEntity.status(status).body(problemDetail);
+    }
+
+    @ExceptionHandler(CategoryNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleCategoryNotFoundException(CategoryNotFoundException exception) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
+        problemDetail.setTitle("Category Not Found");
+        problemDetail.setType(URI.create(GlobalExceptionHandler.DOMAIN_URI + "/category-not-found"));
+
+        return ResponseEntity.status(status).body(problemDetail);
+    }
 }
